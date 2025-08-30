2df47da60d720b076d9ba137fcd328453838340c
naveenbish <bishtnitin003@gmail.com>
2025-08-30 13:14:31 +0530

    cn added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,16 @@
 import HeaderComponent from "@/components/headerComponent";
 import { Counter } from "@/features/counter/Counter";
 import Pokemon from "@/features/pokemon/Pokemon";
+import { Container } from "@/components/Container";
 
 export default function Home() {
   return (
     <div className="py-10">
-      <HeaderComponent />
-      <Counter />
-      <Pokemon />
+      <Container>
+        <HeaderComponent />
+        <Counter />
+        <Pokemon />
+      </Container>
     </div>
   )
 }
